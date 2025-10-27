702e16e4b0c7da4a49c818dfe5fa07789baecaea
vuxuanbach46-cpu <vuxuanbach46@gmail.com>
2025-10-28 01:26:46 +0700

    Update bai6ss4.c

--- bai6ss4.c
+++ bai6ss4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main (){
-	int sdc ,sdm , sodien ;
+	int sdc ,sdm , soDien ;
 	long tien ;
 	
 	printf ("nhap so dien cu :");
@@ -9,17 +9,17 @@ int main (){
 	printf ("nhap so dien moi :");
 	scanf ("%d",&sdm);
 	
-	sodien = sdm - sdc ;
-	if (sodien>=0 && sodien<50)
-	tien = sodien * 10000;
-	else if (sodien>=50 && sodien<100)
-	tien = sodien * 15000;
-	else if (sodien>=100 && sodien<150)
-	tien = sodien * 20000;
-	else if (sodien>=150 && sodien<200)
-	tien = sodien * 25000;
-	else if (sodien>=200)
-	tien = sodien * 30000;
+	soDien = sdm - sdc ;
+	if (soDien < 50)
+        tien = soDien * 10000;
+    else if (soDien < 100)
+        tien = 50 * 10000 + (soDien - 50) * 15000;
+    else if (soDien < 150)
+        tien = 50 * 10000 + 50 * 15000 + (soDien - 100) * 20000;
+    else if (soDien < 200)
+        tien = 50 * 10000 + 50 * 15000 + 50 * 20000 + (soDien - 150) * 25000;
+    else
+        tien = 50 * 10000 + 50 * 15000 + 50 * 20000 + 50 * 25000 + (soDien - 200) * 30000;
 	
 	printf ("tien dien trong thang cua ho gia dinh la :%ld VND",tien);
 	
